5e1cda81f901a3790d81bac8a191bb868c59510c
Usman Ali <usmanali9525@gmail.com>
2023-11-13 12:44:32 +0500

    New

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ import math
 
 start_time = time.time()
 
-image = cv2.imread('dotpeen-applications.jpg', cv2.IMREAD_GRAYSCALE)
+image = cv2.imread('Dot Peen Large.jpg', cv2.IMREAD_GRAYSCALE)
 blurred = cv2.GaussianBlur(image, (5, 5), 0)
 _, binary_image = cv2.threshold(blurred, 0, 255, cv2.THRESH_BINARY + cv2.THRESH_OTSU)
 
