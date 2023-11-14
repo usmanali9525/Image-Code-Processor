8a93080d12c318e2b83ed2d24ef0bd51ed946b2b
Usman Ali <usmanali9525@gmail.com>
2023-11-14 14:41:05 +0500

    new

--- main.py
+++ main.py
@@ -5,16 +5,26 @@ import math
 
 start_time = time.time()
 
-image = cv2.imread('TestingImages\Test2.png', cv2.IMREAD_GRAYSCALE)
-blurred = cv2.GaussianBlur(image, (5, 5), 0)
-_, binary_image = cv2.threshold(blurred, 0, 255, cv2.THRESH_BINARY + cv2.THRESH_OTSU)
+image = cv2.imread('TestingImages\Test1.jpg', cv2.IMREAD_GRAYSCALE)
+#cv2.imshow('Image', image)
 
-
-decoded_objects = decode(binary_image)
+decoded_objects = decode(image)
 
 if decoded_objects:
     for obj in decoded_objects:
         print(f'Decoded Data Matrix: {obj.data.decode("utf-8")}')
+
+elif not decoded_objects:
+    blurred = cv2.GaussianBlur(image, (5, 5), 0)
+    _, binary_image = cv2.threshold(blurred, 0, 255, cv2.THRESH_BINARY + cv2.THRESH_OTSU)
+    decoded_objects2 = decode(binary_image)
+
+    if decoded_objects2:
+        for obj in decoded_objects2:
+            print(f'Decoded Data Matrix: {obj.data.decode("utf-8")}')
+    else:
+        print('No Data Matrix code found in the image.')
+
 else:
     print('No Data Matrix code found in the image.')
 
@@ -22,5 +32,5 @@ end_time = time.time()
 elapsed_time_ms = (end_time - start_time) * 1000
 print(f'Time taken to decode the image: {math.ceil(elapsed_time_ms)} milliseconds')
 
-# cv2.imshow('image', binary_image)
-# cv2.waitKey(0)
+
+cv2.waitKey(0)
