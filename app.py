cbedc5fc1dce2f11ac9f4169b4348f38d3f24c5d
Usman Ali <usmanali9525@gmail.com>
2023-11-23 12:58:22 +0500

    new

---
+++ app.py
@@ -0,0 +1,82 @@
+from flask import Flask, request, jsonify, render_template
+import cv2
+from pylibdmtx.pylibdmtx import decode
+from pyzbar import pyzbar
+import base64
+import numpy as np
+import json
+
+app = Flask(__name__)
+
+def process_image(base64_string, processing_type):
+    image_data = base64.b64decode(base64_string)
+    image_np = np.frombuffer(image_data, np.uint8)
+    image = cv2.imdecode(image_np, cv2.IMREAD_GRAYSCALE)
+
+    response_data = []
+
+    if processing_type == '1':
+        decoded_objects = decode(image)
+
+        if decoded_objects:
+            for obj in decoded_objects:
+                barcode_json = {"Decoded Data Matrix": obj.data.decode("utf-8")}
+                response_data.append(barcode_json)
+        else:
+            response_data.append({'message': 'No Data Matrix code found in the image.'})
+
+    elif processing_type == '2':
+        barcodes = pyzbar.decode(image)
+        if barcodes:
+            for barcode in barcodes:
+                barcode_data = barcode.data.decode('utf-8')
+                barcode_json = {"Barcode": barcode_data}
+                response_data.append(barcode_json)
+        else:
+            response_data.append({'message': 'No BarCode Detected'})
+
+    elif processing_type == '3':
+        barcodes = pyzbar.decode(image)
+        if barcodes:
+            for barcode in barcodes:
+                barcode_data = barcode.data.decode('utf-8')
+                barcode_json = {"QRCode": barcode_data}
+                response_data.append(barcode_json)
+        else:
+            response_data.append({'message': 'No QRCode Detected'})
+
+    elif processing_type == '4':
+        blurred = cv2.GaussianBlur(image, (5, 5), 0)
+        _, binary_image = cv2.threshold(blurred, 0, 255, cv2.THRESH_BINARY + cv2.THRESH_OTSU)
+        decoded_objects2 = decode(binary_image)
+
+        if decoded_objects2:
+            for obj in decoded_objects2:
+                barcode_json = {"Decoded Dot Peen Matrix": obj.data.decode("utf-8")}
+                response_data.append(barcode_json)
+        else:
+            response_data.append({'message': 'No Data Matrix code found in the image.'})
+
+    return response_data
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/process_image', methods=['POST'])
+def process_image_api():
+    try:
+        content = request.get_json()
+        base64_string = content['base64_string']
+        processing_type = content['processing_type']
+        #additional_input = content['additional_input']
+
+        response_data = process_image(base64_string, processing_type)
+
+        return jsonify(response_data)
+
+    except Exception as e:
+        return jsonify({'error': str(e)})
+
+if __name__ == '__main__':
+    app.run(debug=True)
--- main.py
+++ main.py
@@ -5,14 +5,13 @@ import base64
 import numpy as np
 import json
 
-# base64_string = "iVBORw0KGgoAAAANSUhEUgAAAgAAAAIAAQMAAADOtka5AAAABlBMVEX///8AAABVwtN+AAAACXBIWXMAAA7EAAAOxAGVKw4bAAACAklEQVR42u3cPY6DMBCG4UFbUOYIOQpHS46Wo3AEyhQIr7HHszZa8lPFOO9XRJHsfbZiNAw4IoQQQgghhBBCCDlGflyemwz6TTr/MYctfbHlCgAAUC1wt+9nD8T4jYst926yLQMAAEDbQKwYowIXvycUlMUvzqGgTHKyegMAAHAcYCUUEO0PAAAAvhrwf975cpL6AwAAgGMAZbtv/YHLgHfuFwAAAA4LbMeBV0+IFAXlvXkiAADAZ4BtBt0TNs9FuXgtAAAAhwXWgnIPtwP5ODAUFMnGgY/7AwAAgDqAeMFHYlDCf3RaEXr1UwMhAAAA7QJus+OS+oNFF9f7hclXm/FJRQIAAPg0kPr5s9WDjb4WixXYmx8AAAA0AqScipmfAc/vFwAAAOoBYj8/6XoCMi1WhDEs3/753wAAAM0AojcE8eH9UBaUmCmrN1cAAIBqgV4veBcu+TX68D4Bd2v3b7v9AQAAQCOAy4aBEQhZijd9RiUEAACgWqBMfDfPTtY5e5YHAADQPLB9V7d4mUeyozij+QAAALUCuydptvUAAADgC4D8pO3w9ypPV9SacaehAAAAqBvIHtblB2UH516sBwAAAA0A1h/ExTkb/scAAADUDKScs4PzItnv2p5sug8AANA0UI4DrT9IP6SRBgCPzvIAAADUABBCCCGEEEIIIaTZ/ALesjYwnLBumgAAAABJRU5ErkJggg=="
+base64_string = "Base64 Data"
 
-# # Extract the base64 encoded image data
-# image_data = base64.b64decode(base64_string)
-# image_np = np.frombuffer(image_data, np.uint8)
-# image = cv2.imdecode(image_np, cv2.IMREAD_GRAYSCALE)
+image_data = base64.b64decode(base64_string.split(',')[1])
+image_np = np.frombuffer(image_data, np.uint8)
+image = cv2.imdecode(image_np, cv2.IMREAD_GRAYSCALE)
 
-image = cv2.imread('TestingImages\Test5.png', cv2.IMREAD_GRAYSCALE)
+#image = cv2.imread('TestingImages\Test5.png', cv2.IMREAD_GRAYSCALE)
 
 while True:
     print('Press 1 for DataMatrix')
@@ -46,7 +45,7 @@ while True:
                 json_output = json.dumps(barcodeJson, indent=2)
                 print(json_output)
         else: 
-            print('No  BarCode Detected')
+            print('No BarCode Detected')
     elif Number == 3:
         barcodes = pyzbar.decode(image) 
         
